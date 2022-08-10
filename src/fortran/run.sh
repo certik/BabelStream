e0971aa15d6fac2bc1de6e5080b53f7288975fe9
Jeff Hammond <jeff.science@gmail.com>
2022-08-10 22:26:21 +0300

    helper scripts

--- src/fortran/run.sh
+++ src/fortran/run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-#for compiler in gcc oneapi nvhpc ; do
+export KMP_HW_PLACES=1s,4c,1t
+export KMP_HW_SUBSET=1s,4c,1t
+export KMP_AFFINITY=compact,granularity=fine #,verbose
+
+# CPU
+for compiler in oneapi ; do
+    for implementation in DoConcurrent Array OpenMP OpenMPWorkshare OpenMPTaskloop ; do
+        if [ -f BabelStream.${compiler}.${implementation} ] ; then
+            ./BabelStream.${compiler}.${implementation} -s $((1024*1024*64))
+        fi
+    done
+done
+# GPU
 for compiler in nvhpc ; do
-    # CPU
-    #for implementation in DoConcurrent Array OpenMP OpenMPWorkshare OpenMPTaskloop OpenACC OpenACCArray ; do
-    # GPU
     for implementation in DoConcurrent OpenMPTarget OpenMPTargetLoop OpenACC OpenACCArray CUDA CUDAKernel ; do
         if [ -f BabelStream.${compiler}.${implementation} ] ; then
-            ./BabelStream.${compiler}.${implementation} -s $((1024*1024*1024))
+            ./BabelStream.${compiler}.${implementation} -s $((1024*1024*64))
         fi
     done
 done
