de49723a7ae864847a2136353a45a49502291373
Jeff Hammond <jeff.science@gmail.com>
2022-08-10 22:26:23 +0300

    helper scripts

--- src/fortran/build.sh
+++ src/fortran/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-#for compiler in gcc oneapi nvhpc ; do
+for compiler in oneapi ; do
+    for implementation in DoConcurrent Array OpenMP OpenMPWorkshare OpenMPTarget OpenMPTargetLoop OpenMPTaskloop ; do
+        make COMPILER=${compiler} IMPLEMENTATION=${implementation}
+    done
+done
+#exit
 for compiler in nvhpc ; do
     for implementation in DoConcurrent Array OpenMP OpenMPWorkshare OpenMPTarget OpenMPTargetLoop OpenMPTaskloop OpenACC OpenACCArray CUDA CUDAKernel ; do
         make COMPILER=${compiler} IMPLEMENTATION=${implementation}
