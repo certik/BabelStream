d92985239b31e16d478ca3a8a740baba2c35c164
Jeff Hammond <jehammond@nvidia.com>
2022-08-19 02:11:07 -0700

    Xeon stuff

--- src/fortran/build.sh
+++ src/fortran/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-compiler=cray
-for implementation in DoConcurrent Array OpenMP OpenMPWorkshare OpenMPTarget OpenMPTargetLoop OpenMPTaskloop OpenACC OpenACCArray  ; do
-    make COMPILER=${compiler} IMPLEMENTATION=${implementation}
+for compiler in oneapi gcc nvhpc ; do
+    for implementation in DoConcurrent Array OpenMP OpenMPWorkshare OpenMPTarget OpenMPTargetLoop OpenMPTaskloop OpenACC OpenACCArray CUDA CUDAKernel ; do
+        make COMPILER=${compiler} IMPLEMENTATION=${implementation}
+    done
 done
