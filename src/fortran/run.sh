9f4bee439c36b592321f4af38235450cfb23cdf2
Jeff Hammond <jeff.science@gmail.com>
2022-08-23 16:12:13 +0300

    build and run updates

--- src/fortran/run.sh
+++ src/fortran/run.sh
@@ -2,13 +2,25 @@
 
 cat ./run.sh
 
-M=1024
-AFFCONTROL="numactl -N 0 -m 0 -C `seq -s "," 0 31`"
+if [ `uname -s` == Darwin ] ; then
+    NUM_HWTHREADS=`sysctl -n hw.ncpu`
+    MEMORY_BYTES=`sysctl -n hw.memsize`
+else
+    NUM_HWTHREADS=`nproc`
+    MEMORY_KILOS=`grep MemTotal /proc/meminfo | awk '{print $2}'`
+fi
+
+M=128
+
 export OMP_NUM_THREADS=32
 export OMP_PROC_BIND=close
 export OMP_PLACES=threads
 
-for compiler in oneapi gcc nvhpc ; do
+export ACC_NUM_CORES=${OMP_NUM_THREADS}
+
+AFFCONTROL="numactl -N 0 -m 0 -C `seq -s "," 0 ${OMP_NUM_THREADS}`"
+
+for compiler in gcc nvhpc cray oneapi arm amd fj ; do
     for implementation in OpenMP OpenMPTaskloop OpenMPWorkshare DoConcurrent Array OpenACC OpenACCArray CUDA CUDAKernel ; do
         if [ -f BabelStream.${compiler}.${implementation} ] ; then
             echo "BabelStream.${compiler}.${implementation}"
