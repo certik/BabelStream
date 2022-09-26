310965399a9b518122ff610b61419cdaab75ecd0
Jeff Hammond <jehammond@nvidia.com>
2022-09-26 03:39:01 -0700

    because gomp so confict

--- src/fortran/run.sh
+++ src/fortran/run.sh
@@ -12,7 +12,7 @@ fi
 
 M=128
 
-export OMP_NUM_THREADS=12
+export OMP_NUM_THREADS=80
 export OMP_PROC_BIND=close
 export OMP_PLACES=threads
 
@@ -21,9 +21,13 @@ export ACC_NUM_CORES=${OMP_NUM_THREADS}
 AFFCONTROL="numactl -N 0 -m 0 -C `seq -s "," 0 $((${OMP_NUM_THREADS}-1))`"
 
 for compiler in gcc nvhpc cray oneapi arm amd fj ; do
+    #if [ "x$compiler" == "xgcc" ] ; then
+    #   export LD_PRELOAD=/usr/lib/gcc/aarch64-linux-gnu/11/libgomp.so
+    #fi
     for implementation in OpenMP OpenMPTaskloop OpenMPWorkshare DoConcurrent Array OpenACC OpenACCArray CUDA CUDAKernel ; do
         if [ -f BabelStream.${compiler}.${implementation} ] ; then
             echo "BabelStream.${compiler}.${implementation}"
+            ldd BabelStream.${compiler}.${implementation}
             time $AFFCONTROL \
             ./BabelStream.${compiler}.${implementation} -s $((1024*1024*${M}))
         fi
